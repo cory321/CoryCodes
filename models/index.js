c24fe6b0a9aba4642b69b9d190be17b79ce892bb
Cory Williams <cory321@gmail.com>
2015-09-21 23:09:12 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 var mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost/users");
-mongoose.connect("mongodb://cory321:cory103662@ds051953.mongolab.com:51953/heroku_rqzmw3k7");
 
 module.exports.User = require("./user");
 module.exports.Project = require("./project");
